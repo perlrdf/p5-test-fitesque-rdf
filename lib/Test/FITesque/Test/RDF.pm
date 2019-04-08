d1eba58d3028da9b90a64e334595d439e1a61dba
Kjetil Kjernsmo <kjetil@kjernsmo.net>
2019-04-09 01:58:18 +0200

    Implement test class first

---
+++ lib/Test/FITesque/Test/RDF.pm
@@ -0,0 +1,51 @@
+use 5.010001;
+use strict;
+use warnings;
+
+package Test::FITesque::Test::RDF;
+
+our $AUTHORITY = 'cpan:KJETILK';
+our $VERSION   = '0.001';
+
+1;
+
+__END__
+
+=pod
+
+=encoding utf-8
+
+=head1 NAME
+
+Test::FITesque::Test::RDF - Formulate Test::FITesque fixture tables in RDF
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 BUGS
+
+Please report any bugs to
+L<https://github.com/kjetilk/p5-test-fitesque-rdf/issues>.
+
+=head1 SEE ALSO
+
+=head1 AUTHOR
+
+Kjetil Kjernsmo E<lt>kjetilk@cpan.orgE<gt>.
+
+=head1 COPYRIGHT AND LICENCE
+
+This software is Copyright (c) 2019 by Inrupt Inc.
+
+This is free software, licensed under:
+
+  The MIT (X11) License
+
+
+=head1 DISCLAIMER OF WARRANTIES
+
+THIS PACKAGE IS PROVIDED "AS IS" AND WITHOUT ANY EXPRESS OR IMPLIED
+WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF
+MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+
